878a3c5ff41beafc586397312e10c30115eef0c9
Jack Owens <jackowens@jacks-imac.lan>
2018-05-05 13:09:52 +0800

    Todo list complete with datasource and delegate methods

---
+++ TO:DO/TODOListViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  TO:DO
+//
+//  Created by Jack Owens on 1/5/18.
+//  Copyright © 2018 Jack Owens. All rights reserved.
+//
+
+import UIKit
+
+class TODOListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Defeat Demigorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+
+
+}
+
