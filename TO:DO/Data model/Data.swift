47235cdebfec91b0dfdc88bfda2804fd2a5a7cbd
Jack Owens <jackowens@jacks-imac.lan>
2018-06-29 12:22:17 +0800

    Persisting Data using core Data

---
+++ TO:DO/Data model/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  TO:DO
+//
+//  Created by Jack Owens on 17/5/18.
+//  Copyright © 2018 Jack Owens. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = 0
+}
